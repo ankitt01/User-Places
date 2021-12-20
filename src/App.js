d361661e2236b9ba6b542f555ff8bbd91e3ced7b
ankitt01 <ankitrawat0102@gmail.com>
2021-12-20 22:22:15 +0530

    Implemented react router and set up different pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
+import Users from './user/pages/Users'
+import NewPlace from './places/pages/NewPlace';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+      <Router>
+        <Switch>
+          <Route path="/" exact>
+            <Users />
+          </Route>
+          <Route path="/places/new">
+            <NewPlace />
+          </Route>
+          <Redirect to="/" />
+        </Switch>
+      </Router>
   );
 }
 
 export default App;
+ 
